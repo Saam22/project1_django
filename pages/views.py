b8aab467c43f42272e68ec53291a651a14f19af2
saad <saad@gmail.com>
2025-10-17 12:24:31 +0300

    Initial commit

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Login
+from .forms import Loginform
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    x=request.POST.get('username')
+    y=request.POST.get('password')
+    data = Login(username=x, password=y)
+    # data.save()
+    return render(request, 'pages/about.html',{'lf':Loginform})
